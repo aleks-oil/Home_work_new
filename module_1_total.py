9563e7752ed31165fc8db0ebf16409edee71544f
aleks-oil <aleksoil82@gmail.com>
2024-11-28 00:18:53 +0500

    27/11

--- module_1_total.py
+++ module_1_total.py
@@ -7,18 +7,13 @@ print(lst_st)
 res = lst_st.sort()  # преобразованное множество в список выполняем соритровку по алфавиту
 print(lst_st)
 # промежуточные вычисления
-a = [5, 3, 3, 5, 4]
-b = [2, 2, 2, 3]
-c = [4, 5, 5, 2]
-d = [4, 4, 3]
-e = [5, 5, 5, 4, 5]
-a = round(sum(a) / len(a), 1)
-b = round(sum(b) / len(b), 1)
-c = round(sum(c) / len(c), 1)
-d = round(sum(d) / len(d), 1)
-e = round(sum(e) / len(e), 1)
-grades = [a, b, c, d, e]
+grades_average =  ( round(sum(grades[0]) / len(grades[0]), 1),
+                    round(sum(grades[1]) / len(grades[1]), 1),
+                    round(sum(grades[2]) / len(grades[2]), 1),
+                    round(sum(grades[3]) / len(grades[3]), 1),
+                    round(sum(grades[4]) / len(grades[4]), 1),)
+
 # создание словаря
-res = zip(lst_st, grades)  # объединение списка студентов и списка оценок из последовательностей
+res = zip(lst_st, grades_average)  # объединение списка студентов и списка оценок из последовательностей
 book_teacher = dict(res)  # преобразование нового списка в словарь
 print(book_teacher)
